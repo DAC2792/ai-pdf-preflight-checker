6e0c5ff5dcb22a380cf64c73ca550db6621ae700
David <davidamorycrawford@gmail.com>
2026-06-20 20:53:51 +0100

    Connect extraction and rules engine via main.py for resolution check

---
+++ src/main.py
@@ -0,0 +1,15 @@
+from extraction import open_pdf
+from rules_engine import load_rules, check_resolution
+
+if __name__ == "__main__":
+
+    #Acquire results from open_pdf, and rules from the yaml file
+    extraction_results = open_pdf("sample_pdfs/low_res_sample.pdf")
+    rules = load_rules("config/preflight_rules.yaml")
+
+    #Loop through each image in the open_pdf extraction, and produce the dpi check results
+    for item in extraction_results:
+        page_number = item["page"]
+        dpi = item["dpi"]
+        result = check_resolution(dpi, page_number, rules)
+        print(result)

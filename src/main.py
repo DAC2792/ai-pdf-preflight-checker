92b672323445f3ed3cbdfe98ee33194b616fe71c
David <davidamorycrawford@gmail.com>
2026-06-22 20:54:58 +0100

    Add user input for filepath and overall pass/fail verdict

--- src/main.py
+++ src/main.py
@@ -16,6 +16,9 @@ if __name__ == "__main__":
     extraction_results = open_pdf(filepath)
     rules = load_rules("config/preflight_rules.yaml")
 
+    #List created to store all results in
+    pdf_results = []
+
     #Loop through each image in the open_pdf extraction/rules_engine, and produce the check results
     for item in extraction_results:
         if item["check_type"] == "image":
@@ -24,15 +27,24 @@ if __name__ == "__main__":
             print_mode = item["print_mode"]
             resolution_result = check_resolution(dpi, page_number, rules)
             colour_result = check_colour_mode(print_mode, page_number, rules)
-            print(resolution_result)
-            print(colour_result)
+            pdf_results.append(resolution_result)
+            pdf_results.append(colour_result)
         elif item["check_type"] == "bleed":
             page_number = item["page"]
             bleed_data = item["bleed_data"]
             bleed_result = check_bleed(bleed_data, page_number, rules)
-            print(bleed_result)
+            pdf_results.append(bleed_result)
         elif item["check_type"] == "fonts":
             page_number = item["page"]
             font_data = item["font_data"]
             fonts_result = check_fonts(font_data, page_number, rules)
-            print(fonts_result)
+            pdf_results.append(fonts_result)
+
+    #Loop through all results and either Pass or Fail depending on results. Any fails will be fed back to the user along with the page location
+    if all(item["result"] == "pass" for item in pdf_results):
+        print("PASS - this PDF is print ready!")
+    else:
+        print("FAIL - this PDF is not print ready.")
+        for item in pdf_results:
+            if item["result"] == "fail":
+                print(f" - {item['check']} failed on page {item['page']}")
